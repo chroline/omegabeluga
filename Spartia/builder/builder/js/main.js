e393fe8f309edd81e3d98a9e42ca53ecd6153584
Cole Gawin <colegawin@gmail.com>
2021-01-25 23:01:43 -0600

    Update main.js

--- Spartia/builder/builder/js/main.js
+++ Spartia/builder/builder/js/main.js
@@ -1,12 +1,5 @@
 /**
- * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2015, Codrops
- * http://www.codrops.com
+ * HOLA BRANCH
  */
 (function() {
 
